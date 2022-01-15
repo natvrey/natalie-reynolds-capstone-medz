66641dccc869f6216e94b67a9d8d69be5485ca7d
Natalie Reynolds <natalievreynolds@gmail.com>
2022-01-14 21:04:29 -0800

    Create & test GET all & single profile(s), DELETE & POST profile endpoints

---
+++ server/routes/profiles.js
@@ -0,0 +1,123 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const { v4: uuid } = require("uuid");
+
+const readFile = () => {
+  const profilesData = fs.readFileSync("./data/profiles.json");
+
+  return JSON.parse(profilesData);
+};
+
+const writeFile = (profilesData) => {
+  fs.writeFileSync(
+    "./data/profiles.json",
+    JSON.stringify(profilesData, null, 2)
+  );
+};
+
+router.get("/", (req, res) => {
+  let profilesData = readFile();
+
+  let newProfilesData = [...profilesData];
+
+  newProfilesData = newProfilesData.map((newProfile) => {
+    const {
+      photo,
+      middleName,
+      lastName,
+      gender,
+      birthday,
+      bloodType,
+      height,
+      weight,
+      conditions,
+      medications,
+      allergies,
+      doctor,
+      contacts,
+      notes,
+      timestamp,
+      id,
+      ...listProfilesKeys
+    } = newProfile;
+    return listProfilesKeys;
+  });
+
+  return res.status(200).send(newProfilesData);
+});
+
+router.get("/:id", (req, res) => {
+  const profilesData = readFile();
+  const profile = profilesData.find((profile) => profile.id === req.params.id);
+
+  if (!profile) {
+    return res.status(404).send("Profile not found");
+  }
+
+  return res.status(200).json(profile);
+});
+
+router.post("/", (req, res) => {
+  if (
+    !req.body.firstName ||
+    !req.body.lastName ||
+    !req.body.birthday ||
+    !req.body.conditions ||
+    !req.body.medications ||
+    !req.body.allergies ||
+    !req.body.contacts
+  ) {
+    return res
+      .status(400)
+      .send(
+        "Please make sure to include your first & last name, DOB, conditions, medications,allergies and emergency contact(s)"
+      );
+  }
+
+  const newProfile = {
+    photo: "/images/avatar-placeholder-medz.png",
+    firstName: req.body.firstName,
+    middleName: req.body.middleName,
+    lastName: req.body.lastName,
+    gender: req.body.gender,
+    birthday: req.body.birthday,
+    bloodType: req.body.bloodType,
+    height: req.body.height,
+    weight: req.body.weight,
+    conditions: req.body.conditions,
+    medications: req.body.medications,
+    allergies: req.body.allergies,
+    doctor: req.body.doctor,
+    contacts: req.body.contacts,
+    notes: req.body.notes,
+    timestamp: Date.now(),
+    id: uuid(),
+  };
+
+  const profilesData = readFile();
+  profilesData.push(newProfile);
+  writeFile(profilesData);
+
+  return res.status(201).json(newProfile);
+});
+
+router.delete("/:id", (req, res) => {
+  let profilesData = readFile();
+  const foundProfile = profilesData.find(
+    (profile) => profile.id === req.params.id
+  );
+
+  if (!foundProfile) {
+    return res.status(404).send("Profile not found");
+  }
+
+  profilesData = profilesData.filter(
+    (profile) => profile.id !== foundProfile.id
+  );
+  writeFile(profilesData);
+
+  res.status(204).send();
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const profilesRouter = require("./routes/profiles");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8082;
+
+app.use(cors({}));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/profiles", profilesRouter);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on ${PORT}`);
+});
