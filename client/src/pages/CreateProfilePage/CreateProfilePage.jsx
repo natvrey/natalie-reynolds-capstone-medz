8acefb424b9d7bb98e09b8e8fa036803454fd7c2
Natalie Reynolds <92003973+natvrey@users.noreply.github.com>
2022-01-18 22:26:42 -0800

    Give input & text area tags their proper names,ids, placeholders

--- client/src/pages/CreateProfilePage/CreateProfilePage.jsx
+++ client/src/pages/CreateProfilePage/CreateProfilePage.jsx
@@ -46,10 +46,10 @@ const CreateProfilePage = (props) => {
                 htmlFor="photo"
                 className="create-profile__input-title create-profile__label"
               >
-                Profile photo <br></br>
+                Profile photo
                 <input
                   type="text"
-                  id="videoTitle"
+                  id="photo"
                   placeholder="Click here to upload a photo"
                   onFocus={(e) => {
                     e.target.placeholder = "";
@@ -66,22 +66,21 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoTitle"
+                htmlFor="firstName"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                First name:
                 <input
                   type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  id="firstName"
+                  placeholder="Enter your first name"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "Enter your first name";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="firstName"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -90,22 +89,21 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoTitle"
+                htmlFor="middleName"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                Middle name:
                 <input
                   type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  id="lastName"
+                  placeholder="Enter your middle name"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "Enter your middle name";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="middleName"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -114,22 +112,21 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoTitle"
+                htmlFor="lastName"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                Last name:
                 <input
                   type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  id="lastName"
+                  placeholder="Enter your last name"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "Enter your last name";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="lastName"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -138,22 +135,21 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoTitle"
+                htmlFor="gender"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                Gender
                 <input
                   type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  id="gender"
+                  placeholder="This field is optional"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "This field is optional";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="gender"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -166,19 +162,18 @@ const CreateProfilePage = (props) => {
                 htmlFor="videoTitle"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                Date of Birth
                 <input
-                  type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  type="birthday"
+                  id="birthday"
+                  placeholder="(MM/DD/YYYY)"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "(MM/DD/YYYY)";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="birthday"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -187,22 +182,21 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoTitle"
+                htmlFor="bloodType"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                Blood Type
                 <input
                   type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  id="bloodType"
+                  placeholder="This field is optional"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "This field is optional";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="bloodType"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -211,22 +205,21 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoTitle"
+                htmlFor="weight"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                Weight
                 <input
                   type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  id="weight"
+                  placeholder="e.g. 160lbs OR 72.5kg"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "e.g. 160lbs, OR 72.5kg";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="weight"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -235,22 +228,21 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoTitle"
+                htmlFor="height"
                 className="create-profile__input-title uploads__label"
               >
-                TITLE YOUR VIDEO <br></br>
+                Height
                 <input
                   type="text"
-                  id="videoTitle"
-                  placeholder="Add a title to your video"
+                  id="height"
+                  placeholder="e.g. 5 feet 11 inches, OR 180cm"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a title to your video";
+                    e.target.placeholder = "e.g. 5 feet 11 inches, OR 180cm";
                   }}
-                  name="title"
-                  aria-label="Search through website content"
+                  name="height"
                   className="create-profile__input"
                   required
                   minLength="4"
@@ -259,16 +251,97 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoDescription"
+                htmlFor="conditions"
                 className="create-profile__input-title create-profile__label"
               >
-                ADD A VIDEO DESCRIPTION
+                <p>
+                  {" "}
+                  Medical <br></br>conditions
+                </p>
                 <textarea
                   className="uploads__textarea"
-                  id="videoDescription"
-                  name="description"
+                  id="conditions"
+                  name="conditions"
                   type="text"
-                  placeholder="Add a description to your video"
+                  placeholder="e.g. Diabetes, Epilepsy, Asthma"
+                  onFocus={(e) => {
+                    e.target.placeholder = "";
+                  }}
+                  onBlur={(e) => {
+                    e.target.placeholder = "e.g. Diabetes, Epilepsy, Asthma";
+                  }}
+                  rows="4"
+                  cols="10"
+                  required
+                  minLength="4"
+                ></textarea>{" "}
+              </label>
+            </div>
+            <div className="create-profile__input-container">
+              <label
+                htmlFor="medications"
+                className="create-profile__input-title create-profile__label"
+              >
+                Medications
+                <textarea
+                  className="uploads__textarea"
+                  id="medications"
+                  name="medications"
+                  type="text"
+                  placeholder="List all medications e.g. Levothyroxine 50mcg, Insulin."
+                  onFocus={(e) => {
+                    e.target.placeholder = "";
+                  }}
+                  onBlur={(e) => {
+                    e.target.placeholder =
+                      "List all medications e.g. Levothyroxine 50mcg, Insulin.";
+                  }}
+                  rows="4"
+                  cols="10"
+                  required
+                  minLength="4"
+                ></textarea>{" "}
+              </label>
+            </div>
+            <div className="create-profile__input-container">
+              <label
+                htmlFor="allergies"
+                className="create-profile__input-title create-profile__label"
+              >
+                Allergies
+                <textarea
+                  className="uploads__textarea"
+                  id="allergies"
+                  name="allergies"
+                  type="text"
+                  placeholder="List all allergies & reactions e.g. Peanuts (hives), Seafood (anaphylaxis)"
+                  onFocus={(e) => {
+                    e.target.placeholder = "";
+                  }}
+                  onBlur={(e) => {
+                    e.target.placeholder =
+                      "List all allergies & reactions e.g. Peanuts (hives), Seafood (anaphylaxis)";
+                  }}
+                  rows="4"
+                  cols="10"
+                  required
+                  minLength="4"
+                ></textarea>{" "}
+              </label>
+            </div>
+            <div className="create-profile__input-container">
+              <label
+                htmlFor="doctor"
+                className="create-profile__input-title create-profile__label"
+              >
+                Family doctor
+                <textarea
+                  className="uploads__textarea"
+                  id="doctor"
+                  name="doctor"
+                  type="text"
+                  placeholder="e.g. Dr. Sandra Yap,Apex Medical, 4-2 Molynes Rd,
+                  Kingston, Jamaica, ph: +1 876-960-7905"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
@@ -284,21 +357,22 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoDescription"
+                htmlFor="contacts"
                 className="create-profile__input-title create-profile__label"
               >
-                ADD A VIDEO DESCRIPTION
+                Emergency contacts
                 <textarea
                   className="uploads__textarea"
-                  id="videoDescription"
-                  name="description"
+                  id="contacts"
+                  name="contacts"
                   type="text"
-                  placeholder="Add a description to your video"
+                  placeholder="Name & ph# of your emergency contact(s)"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a description to your video";
+                    e.target.placeholder =
+                      "Name & ph# of your emergency contact(s)";
                   }}
                   rows="4"
                   cols="10"
@@ -309,71 +383,22 @@ const CreateProfilePage = (props) => {
             </div>
             <div className="create-profile__input-container">
               <label
-                htmlFor="videoDescription"
+                htmlFor="notes"
                 className="create-profile__input-title create-profile__label"
               >
-                ADD A VIDEO DESCRIPTION
+                Other notes
                 <textarea
                   className="uploads__textarea"
-                  id="videoDescription"
-                  name="description"
+                  id="notes"
+                  name="notes"
                   type="text"
-                  placeholder="Add a description to your video"
+                  placeholder="e.g. Please call 911 & then call/text an emergency contact"
                   onFocus={(e) => {
                     e.target.placeholder = "";
                   }}
                   onBlur={(e) => {
-                    e.target.placeholder = "Add a description to your video";
-                  }}
-                  rows="4"
-                  cols="10"
-                  required
-                  minLength="4"
-                ></textarea>{" "}
-              </label>
-            </div>
-            <div className="create-profile__input-container">
-              <label
-                htmlFor="videoDescription"
-                className="create-profile__input-title create-profile__label"
-              >
-                ADD A VIDEO DESCRIPTION
-                <textarea
-                  className="uploads__textarea"
-                  id="videoDescription"
-                  name="description"
-                  type="text"
-                  placeholder="Add a description to your video"
-                  onFocus={(e) => {
-                    e.target.placeholder = "";
-                  }}
-                  onBlur={(e) => {
-                    e.target.placeholder = "Add a description to your video";
-                  }}
-                  rows="4"
-                  cols="10"
-                  required
-                  minLength="4"
-                ></textarea>{" "}
-              </label>
-            </div>
-            <div className="create-profile__input-container">
-              <label
-                htmlFor="videoDescription"
-                className="create-profile__input-title create-profile__label"
-              >
-                ADD A VIDEO DESCRIPTION
-                <textarea
-                  className="uploads__textarea"
-                  id="videoDescription"
-                  name="description"
-                  type="text"
-                  placeholder="Add a description to your video"
-                  onFocus={(e) => {
-                    e.target.placeholder = "";
-                  }}
-                  onBlur={(e) => {
-                    e.target.placeholder = "Add a description to your video";
+                    e.target.placeholder =
+                      "e.g. Please call 911 & then call/text an emergency contact";
                   }}
                   rows="4"
                   cols="10"
