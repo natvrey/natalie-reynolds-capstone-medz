9d543a5ee42cefdd65d61e896d52bfb2c4069d12
Natalie Reynolds <92003973+natvrey@users.noreply.github.com>
2022-01-23 15:25:40 -0800

    Put Twilio voice on same port as the app's main server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const profilesRouter = require("./routes/profiles");
+// const express = require("express");
+// const cors = require("cors");
+// const profilesRouter = require("./routes/profiles");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const app = express();
-const PORT = process.env.PORT || 8084;
+// const app = express();
+// const PORT = process.env.PORT || 8084;
 
-app.use(cors({}));
+// app.use(cors({}));
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.use("/profiles", profilesRouter);
+// app.use("/profiles", profilesRouter);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Main Server listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Main Server listening on ${PORT}`);
+// });
